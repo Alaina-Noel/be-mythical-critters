8e0bba615f9135fd03f2cb306860835cce9b4c42
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-17 20:26:49 -0700

    iterator methods

--- exercises/play.js
+++ exercises/play.js
@@ -15,8 +15,28 @@ const useEach = (words) => {
     console.log(word, word.length));
 }
 
+const useFind = (words) => {
+  return words.find(word => word.length === 6);
+}
+
+const useAny = (words) => {
+  return words.some(word => word.length === 6);
+}
+
+const useFilter = (words) => {
+  return words.filter(word => word.length > 6);
+}
+
+const useEvery = (words) => {
+  return words.every(word => word.length >= 6);
+}
+
 nouns = ["people", "places", "thingeeees"]
 
 console.log(capitalize(nouns));
 console.log(useMap(nouns));
 console.log(useEach(nouns));
+console.log(useFind(nouns));
+console.log(useAny(nouns));
+console.log(useFilter(nouns));
+console.log(useEvery(nouns));
